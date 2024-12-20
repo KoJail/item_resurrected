22e63b8c51098776ac9232ecab3231717efd83e2
KoJail <2949306830@qq.com>
2024-12-20 15:13:10 +0800

    还需写跳转至register界面，跳转至主界面

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+import sqlite3
+from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox, QLineEdit
 
 from loginW import Ui_LoginWindow
 
@@ -10,15 +11,61 @@ class LoginWindow(QWidget):
         super().__init__()
         self.ui = Ui_LoginWindow()
         self.ui.setupUi(self)
+        self.ui.password_lineEdit.setEchoMode(QLineEdit.Password)
+
+        # 数据库相关操作
+        conn = sqlite3.connect('user.db')
+        cur = conn.cursor()
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS admin (  
+                        admin_id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                        admin_name TEXT NOT NULL,  
+                        password TEXT NOT NULL
+                    )  
+                ''')
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS users (  
+                        user_id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                        user_name TEXT NOT NULL,  
+                        password TEXT NOT NULL,
+                        address TEXT,
+                        phone TEXT NOT NULL
+                    )  
+                ''')
+        cur.execute("SELECT * from admin")
+        if cur.fetchall() == []:
+            cur.execute("INSERT INTO admin (admin_name,password) VALUES ('root','root')")
+        conn.commit()
+        conn.close()
 
         # 信号与槽
-        self.ui.login_pushButton.clicked.connect((self.login()))
-        self.ui.register_pushButton.clicked.connect((self.register()))
+        self.ui.login_pushButton.clicked.connect((self.login))
+        self.ui.register_pushButton.clicked.connect((self.register))
 
         self.show()
 
     def login(self):
-        return
+        userName = self.ui.userName_lineEdit.text()
+        password = self.ui.password_lineEdit.text()
+        if userName == '' or password == '':
+            QMessageBox.information(self, "消息对话框", "请填写用户名和密码", QMessageBox.Ok, QMessageBox.Ok)
+        else:
+            conn = sqlite3.connect('user.db')
+            cur = conn.cursor()
+
+            cur.execute("SELECT password FROM admin WHERE admin_name=?", (userName,))
+            admin_pas = cur.fetchone()
+            cur.execute("SELECT password FROM users WHERE user_name=?", (userName,))
+            users_pas = cur.fetchone()
+
+            if password == admin_pas[0]: # 管理员登录
+                print('admin sucess')
+            elif password == users_pas[0]: # 普通用户登录
+                print('user sucess')
+            else:
+                QMessageBox.information(self, "消息对话框", "密码错误！", QMessageBox.Ok, QMessageBox.Ok)
+
+            conn.close()
 
     def register(self):
         return
--- loginW.py
+++ loginW.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'login.ui'
+# Form implementation generated from reading ui file 'loginW.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -16,22 +16,22 @@ class Ui_LoginWindow(object):
         LoginWindow.setObjectName("LoginWindow")
         LoginWindow.resize(400, 300)
         self.register_pushButton = QtWidgets.QPushButton(LoginWindow)
-        self.register_pushButton.setGeometry(QtCore.QRect(70, 200, 111, 28))
+        self.register_pushButton.setGeometry(QtCore.QRect(50, 200, 131, 28))
         self.register_pushButton.setObjectName("register_pushButton")
         self.login_pushButton = QtWidgets.QPushButton(LoginWindow)
-        self.login_pushButton.setGeometry(QtCore.QRect(222, 200, 111, 28))
+        self.login_pushButton.setGeometry(QtCore.QRect(220, 200, 131, 28))
         self.login_pushButton.setObjectName("login_pushButton")
         self.userName_label = QtWidgets.QLabel(LoginWindow)
-        self.userName_label.setGeometry(QtCore.QRect(70, 70, 72, 15))
+        self.userName_label.setGeometry(QtCore.QRect(50, 70, 72, 15))
         self.userName_label.setObjectName("userName_label")
         self.password_label = QtWidgets.QLabel(LoginWindow)
-        self.password_label.setGeometry(QtCore.QRect(70, 130, 72, 15))
+        self.password_label.setGeometry(QtCore.QRect(50, 130, 72, 15))
         self.password_label.setObjectName("password_label")
         self.userName_lineEdit = QtWidgets.QLineEdit(LoginWindow)
-        self.userName_lineEdit.setGeometry(QtCore.QRect(140, 65, 191, 21))
+        self.userName_lineEdit.setGeometry(QtCore.QRect(120, 65, 231, 21))
         self.userName_lineEdit.setObjectName("userName_lineEdit")
         self.password_lineEdit = QtWidgets.QLineEdit(LoginWindow)
-        self.password_lineEdit.setGeometry(QtCore.QRect(140, 125, 191, 21))
+        self.password_lineEdit.setGeometry(QtCore.QRect(120, 125, 231, 21))
         self.password_lineEdit.setObjectName("password_lineEdit")
 
         self.retranslateUi(LoginWindow)
