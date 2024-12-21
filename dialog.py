63e980baeb5e7d4a0487ec7f387e95a91471de1b
KoJail <2949306830@qq.com>
2024-12-21 22:21:05 +0800

    写红温了

--- dialog.py
+++ dialog.py
@@ -1,58 +1,79 @@
+import sqlite3
 import sys
 
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QApplication, QHBoxLayout, QLabel, QMessageBox
+
 
 class InputDialog(QDialog):
-    def __init__(self):
+    def __init__(self, columns, table, mw):
         super().__init__()
+
+        self.columns = columns
+        self.table = table
+        self.mw = mw
         self.initUI()
 
     def initUI(self):
         # 创建垂直布局
         layout = QVBoxLayout()
+        self.columnNum = len(self.columns) - 1
+        self.hboxs = []
+        self.lineEdits = []
 
         # 创建多个输入框
-        self.lineEdit1 = QLineEdit(self)
-        self.lineEdit2 = QLineEdit(self)
-        self.lineEdit3 = QLineEdit(self)
+        for i in range(self.columnNum):
+            hbox = QHBoxLayout()
+            label = QLabel(self.columns[i+1])
+            lineEdit = QLineEdit()
+            hbox.addWidget(label)
+            hbox.addWidget(lineEdit)
+            self.hboxs.append(hbox)
+            self.lineEdits.append(lineEdit)
+        for hbox in self.hboxs:
+            layout.addLayout(hbox)
 
-        # 创建确定按钮
-        okButton = QPushButton('OK', self)
-        okButton.clicked.connect(self.on_ok)
+        # 创建按钮
+        self.okButton = QPushButton('确定', self)
+        self.cancelButton = QPushButton('取消', self)
+        self.okButton.clicked.connect(self.okey)
+        self.cancelButton.clicked.connect(self.quit)
+        layout.addWidget(self.okButton)
+        layout.addWidget(self.cancelButton)
 
-        # 将输入框和按钮添加到布局中
-        layout.addWidget(self.lineEdit1)
-        layout.addWidget(self.lineEdit2)
-        layout.addWidget(self.lineEdit3)
-        layout.addWidget(okButton)
-
-        # 设置布局
         self.setLayout(layout)
-        self.setWindowTitle('Multi Input Dialog')
+        self.setWindowTitle('添加物品')
+        self.setWindowFlags(Qt.CustomizeWindowHint | Qt.WindowCloseButtonHint)
 
-    def on_ok(self):
-        # 获取输入框的内容
-        input1 = self.lineEdit1.text()
-        input2 = self.lineEdit2.text()
-        input3 = self.lineEdit3.text()
+    def okey(self):
+        data = ()
+        isEmpty = False
+        for lineEdit in self.lineEdits:
+            data += (lineEdit.text(),)
+        for da in data:
+            if da == '':
+                isEmpty = True
+                QMessageBox.information(self, "消息对话框", "请输入完整信息", QMessageBox.Ok, QMessageBox.Ok)
+                break
+        if isEmpty == False:
+            # 数据库操作
+            conn = sqlite3.connect('item.db')
+            cur = conn.cursor()
+            self.columns.pop(0)
+            # 构建插入语句
+            columns_str = ', '.join(self.columns)
+            placeholders_str = ', '.join(['?'] * self.columnNum)
+            insert_sql = f"INSERT INTO {self.table} ({columns_str}) VALUES ({placeholders_str})"
+            # 执行插入操作
+            cur.execute(insert_sql, data)
+            conn.commit()
+            conn.close()
+            QMessageBox.information(self, "消息对话框", "操作成功！即将为您跳转回主界面", QMessageBox.Ok, QMessageBox.Ok)
+            self.quit()
+            self.mw.displayItem()
 
-        # 可以在这里处理输入的内容
-        print(f"Input 1: {input1}, Input 2: {input2}, Input 3: {input3}")
+    def quit(self):
+        super().reject()
 
-        # 关闭对话框
-        self.close()
-
-# 仅用于内部测试
-if __name__ == '__main__':
-    # 只有直接运行这个脚本，才会往下执行
-    # 别的脚本文件执行，不会调用这个条件句
-
-    # 实例化，传参
-    app = QApplication(sys.argv)
-
-    # 创建对象
-    appWindow = InputDialog()
-    appWindow.show()
-
-    # 进入程序的主循环，并通过exit函数确保主循环安全结束(该释放资源的一定要释放)
-    sys.exit(app.exec_())
+    def closeEvent(self, event):
+        super().closeEvent(event)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sqlite3
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHeaderView, QAbstractItemView
 
 from login import LoginWindow, RegisterWindow
+from dialog import InputDialog
 from mainW import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -12,7 +13,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setWindowTitle('resurrection of items')
+        # QTableWidget设置整行选中
+        self.ui.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.ui.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
 
         # 添加一些表格，默认课程要求的3中类型
         conn = sqlite3.connect('item.db')
@@ -23,10 +26,10 @@ class MainWindow(QMainWindow):
                         名称 TEXT NOT NULL,
                         保质期 TEXT NOT NULL,
                         数量 TEXT NOT NULL,
-                        描述 TEXT NOT NULL,
                         地址 TEXT NOT NULL,
                         手机 TEXT NOT NULL,
-                        邮箱 TEXT
+                        邮箱 TEXT NOT NULL,
+                        描述 TEXT NOT NULL
                     )  
                 ''')
         cur.execute('''
@@ -35,20 +38,20 @@ class MainWindow(QMainWindow):
                         名称 TEXT NOT NULL,
                         作者 TEXT NOT NULL,
                         出版社 TEXT NOT NULL,
-                        描述 TEXT NOT NULL,
                         地址 TEXT NOT NULL,
                         手机 TEXT NOT NULL,
-                        邮箱 TEXT
+                        邮箱 TEXT NOT NULL,
+                        描述 TEXT NOT NULL
                     )  
                 ''')
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS 工具 (  
                         ID INTEGER PRIMARY KEY AUTOINCREMENT,  
                         名称 TEXT NOT NULL,
-                        描述 TEXT NOT NULL,
                         地址 TEXT NOT NULL,
                         手机 TEXT NOT NULL,
-                        邮箱 TEXT
+                        邮箱 TEXT NOT NULL,
+                        描述 TEXT NOT NULL
                     )  
                 ''')
         conn.commit()
@@ -56,14 +59,55 @@ class MainWindow(QMainWindow):
 
         # 信号与槽
         self.ui.add_pushButton.clicked.connect(self.addItem)
+        self.ui.sort_comboBox.currentIndexChanged.connect(self.displayItem)
 
-        self.refresh()
+        self.refreshSort()
 
     def addItem(self):
-        return
+        tableName = self.ui.sort_comboBox.currentText()
+        # 数据库操作
+        conn = sqlite3.connect('item.db')
+        cur = conn.cursor()
+        cur.execute(f"PRAGMA table_info({tableName})") # 查询表的列信息
+        infos = cur.fetchall() # 获取所有列的信息
+        columns = [info[1] for info in infos]
+        conn.commit()
+        conn.close()
+        # 弹出多信息输入窗口
+        self.ipw = InputDialog(columns, tableName, self)
+        self.ipw.show()
 
     # 刷新
-    def refresh(self):
+    def displayItem(self):
+        # 删除原有信息
+        while self.ui.tableWidget.rowCount() > 0:
+            self.ui.tableWidget.removeRow(0)
+        # 数据库操作
+        tableName = self.ui.sort_comboBox.currentText()
+        conn = sqlite3.connect('item.db')
+        cur = conn.cursor()
+        cur.execute(f"PRAGMA table_info({tableName})")  # 查询表的列信息
+        infos = cur.fetchall()  # 获取所有列的信息
+        columns = [info[1] for info in infos]
+        cur.execute(f"SELECT * FROM {tableName}")
+        rows = cur.fetchall()
+        conn.commit()
+        conn.close()
+        # 添加所有信息
+        # 设置表格的行数和列数
+        self.ui.tableWidget.setRowCount(len(rows))
+        self.ui.tableWidget.setColumnCount(len(columns))
+        # 设置表头（可选）
+        self.ui.tableWidget.setHorizontalHeaderLabels(columns)
+        # 将列表数据填充到表格中
+        for row, data in enumerate(rows):
+            for column, value in enumerate(data):
+                self.ui.tableWidget.setItem(row, column, QTableWidgetItem(str(value)))
+        for i in range(len(columns)-1):
+            self.ui.tableWidget.horizontalHeader().setSectionResizeMode(i, QHeaderView.ResizeToContents)  # 设置第i列要根据内容使用宽度
+
+
+    def refreshSort(self):
         # 刷新下拉框中的物品类型
         self.ui.sort_comboBox.clear()
         conn = sqlite3.connect('item.db')
